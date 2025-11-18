e56ac5314f041f9bdc77db2fff353a054f4edfc0
ishisprogrammer <ishdeepverma786@gmail.com>
2025-11-18 21:26:30 +0530

    Update 1) Games.py

--- 1) Games.py
+++ 1) Games.py
@@ -2,95 +2,83 @@ import random
 import numpy as np
 
 def game1():
-   wins = 0
-   losses = 0
-   try:
-       n = int(input('Enter the number of matches you wanna play: '))
-       if n<=0 or n>150:
-           print('Pleae give a valid input\n')
-           return game1()                                                       
-   except:
-          print('Please give a valid input\n')
-          return game1()
-   i = 1
-   while i<=n:
-           if i == 1:
-                print('Enter your 1st choice:')
-           elif i == 2:
-                print('Enter your 2nd choice:')
-           elif i == 3:
-                print('Enter your 3rd choice:')
-           else:
-                print(f'Enter your {i}th choice')
-           comp =  np.random.choice(['stone', 'paper', 'scissor'])
-           user = input().lower()
-           if user not in ['stone', 'paper', 'scissor']:
-               print('Please give a valid input!\n')
-           else:
-               if (user=='stone' and comp=='paper') or (user=='paper' and comp=='scissor') or (user=='scissor' and comp=='stone'):
-                    print(f"The computer chose {comp} and you chose {user}, so you lose this time\n")
-                    losses+=1
-               elif (user=='stone' and comp=='scissor') or (user=='paper' and comp=='stone') or (user=='scissor' and comp=='paper'):
-                    print(f'The computer chose {comp} and you chose {user}, so you won this time\n')
-                    wins+=1
-               else:
-                    print(f"The computer chose {comp} and you chose {user}, so its a draw\n")
-               i+=1
-   else:
-        print(f'There were {wins} wins and {losses} losses\n')
-        if wins>losses:
-             print('Congratulations!\nYou are the winner of this series')
-        elif wins<losses:
-             print('Better luck next time')
+    wins = 0
+    losses = 0
+    while True:
+        try:
+            n = int(input('Enter the number of matches you want to play: '))
+            if n <= 0 or n > 150:
+                print('Please give a valid input\n')
+                continue                                              
+        except (ValueError, TypeError):
+            print('Please give a valid input\n')
+            continue
+    i = 1
+    while True:
+        suffix = ['st' if i == 1 else 'nd' if i == 2 else 'rd' if i == 3 else 'th']
+        print(f'Enter your {i}{suffix[0]} choice')
+        comp = np.random.choice(['stone', 'paper', 'scissor'])
+        user = input().lower()
+        if user not in ['stone', 'paper', 'scissor']:
+            print('Please give a valid input!\n')
+            continue
         else:
-             print('Its a Draw')
+            if (user == 'stone' and comp == 'paper') or (user == 'paper' and comp == 'scissor') or (user == 'scissor' and comp == 'stone'):
+                print(f"The computer chose {comp} and you chose {user}, so you lose this time\n")
+                losses += 1
+            elif (user == 'stone' and comp == 'scissor') or (user == 'paper' and comp == 'stone') or (user == 'scissor' and comp == 'paper'):
+                print(f'The computer chose {comp} and you chose {user}, so you won this time\n')
+                wins += 1
+            else:
+                print(f"The computer chose {comp} and you chose {user}, so it's a draw\n")
+            i += 1
+    else:
+        print(f'There were {wins} wins and {losses} losses\n')
+        if wins > losses:
+            print('Congratulations!\nYou are the winner of this series')
+        elif wins < losses:
+            print('Better luck next time')
+        else:
+            print("It's a draw")
                 
 def game2():
-    i=1
-    comp=np.random.randint(1,150)
-    while i>0:
-         if i==1:
-              print('Enter your 1st choice: ')
-         elif i==2:
-              print('Enter your 2nd choice:')
-         elif i==3:
-              print('Enter your 3rd choice:')
-         else:
-              print(f'Enter your {i}th choice')
-         try:
-              user=int(input())
-         except:
-              print('Please enter a vaid guess\n')
-              continue
-         if user>comp:
-              print('Too high, Guess again!\n')
-              i+=1
-         elif user<comp:
-              print('Too low, Guess again!\n')
-              i+=1
-         else:
-              if i==1:
-                 print(f'Wow!,\nYou guessed the right number in {i} turn')
-                 break
-              else:
-                 print(f'Wow!,\nYou guessed the right number in {i} turns')
-                 break
+    i = 1
+    comp = np.random.randint(1, 150)
+    while True:
+        suffix = ['st' if i == 1 else 'nd' if i == 2 else 'rd' if i == 3 else 'th']
+        print(f'Enter your {i}{suffix[0]} choice')
+        try:
+            user = int(input())
+        except (ValueError, TypeError):
+            print('Please enter a valid guess\n')
+            continue
+        if user > comp:
+            print('Too high, guess again!\n')
+            i += 1
+        elif user < comp:
+            print('Too low, guess again!\n')
+            i += 1
+        else:
+            turn=['turn' if i==1 else 'turns']
+            print(f'Wow!\nYou guessed the right number in {i}{turn[0]}')
+            break
                   
 def start():
-    try:
-       print("Enter the code of the game you wanna play:\n1.) Enter 1 to play 'Stone-Paper-Scissor'\n2.) Enter 2 to play 'Guessing the numbers'")
-       game = input()
-       if int(game) == 1:
-           print('Great!, so lets play the game\n')
-           return game1()
-       elif int(game) == 2:
-           print('Great!, so lets play the game\n')
-           return game2()
-       else:
-           print('Please give a valid input!\n')
-           return start()
-    except:
-           print('Please give a valid input\n')
-           return start()
+    while True:
+        try:
+            print("Enter the code of the game you want to play:\n1.) Enter 1 to play 'Stone-Paper-scissor'\n2.) Enter 2 to play 'Guess the numbers'")
+            game = int(input())
+            if game == 1:
+                print("Great! Let's play the game\n")
+                return game1()
+            elif game == 2:
+                print("Great! Let's play the game\n")
+                return game2()
+            else:
+                print('Please give a valid input!\n')
+                continue
+        except (ValueError, TypeError):
+            print('Please give a valid input\n')
+            continue
 
 start()
